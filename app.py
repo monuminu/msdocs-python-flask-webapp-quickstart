b832f7f80cef4ea648b0121b9d152a949e7e0694
Manoranjan Rajguru <mrajguru@microsoft.com>
2023-12-14 18:28:22 +0530

    adding chanilit

--- app.py
+++ app.py
@@ -1,32 +1,31 @@
+import chainlit as cl
+from langchain.chat_models import ChatOpenAI
+from langchain.schema.runnable.config import RunnableConfig
 import os
+os.environ["OPENAI_API_KEY"] = "59798b794d7a4fab9e6826bf9e28cad5"
 
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+import openai
+openai.api_type = "azure"
+openai.api_base = "https://monuirctc1.openai.azure.com/"
+openai.api_version = "2023-07-01-preview"
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-app = Flask(__name__)
+llm = ChatOpenAI(model_kwargs= {"engine" : "gpt-4-turbo"} , streaming = True)
 
+from util import get_skeleton_prompt_chain
 
-@app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
+@cl.on_chat_start
+def start():
+    chain = get_skeleton_prompt_chain(llm)
+    cl.user_session.set("chain", chain)
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+from langchain.callbacks import StreamingStdOutCallbackHandler
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
-
-if __name__ == '__main__':
-   app.run()
+@cl.on_message
+async def main(message: cl.Message):
+    msg = cl.Message(content="")
+    chain = cl.user_session.get("chain")
+    async for token in chain.astream({"question": message.content}, 
+                                     config = RunnableConfig(callbacks = [StreamingStdOutCallbackHandler(), 
+                                                                          cl.AsyncLangchainCallbackHandler()])):
+        await msg.stream_token(token)
